1c966b7d649e083e9b9fae78c9c0d4186250d385
suman Islam <sumunahmed1416@gmail.com>
2020-10-21 00:11:31 +0600

    added animate.css

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ $(window).scroll(function() {
   let position = $(this).scrollTop();
   // console.log(position);
   if(position >= 250) {
-    $('.mission-card').addClass('from-left');
+    $('.mission-card').addClass('animate__animated animate__fadeIn');
   } else {
-    $('.mission-card').removeClass('from-left');
+    $('.mission-card').removeClass('animate__animated animate__fadeIn');
   }
 
 })
